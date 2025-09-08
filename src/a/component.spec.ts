d86e0fec91e1d01732a2135bac3867d62b0e36d0
Joe Sunley <jsunley@scottlogic.com>
2025-09-08 09:34:03 +0100

    Complete Task A

--- src/a/component.spec.ts
+++ src/a/component.spec.ts
@@ -7,7 +7,15 @@ describe('Calculator', () => {
         component = new Calculator();
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
+    it('should add 1 & 2 to equal 3', () => {
+        expect(component.add(1, 2)).toEqual(3);
     });
+
+    it('should subtract 5 from 13 to equal 8', () => {
+        expect(component.subtract(13, 5)).toEqual(8);
+    })
+
+    it('should sum [1, 2, 3, 4] to equal 10', () => {
+        expect(component.sum([1, 2, 3, 4])).toEqual(10);
+    })
 });
