e490e512edd3602da1f86a2654e8591fab915462
Joe Sunley <jsunley@scottlogic.com>
2025-09-08 10:33:49 +0100

    Complete Task E

--- src/e/component.spec.ts
+++ src/e/component.spec.ts
@@ -1,13 +1,31 @@
-import { Engine } from './component';
+import { Engine, Status } from './component';
 
 describe('Engine', () => {
+    jest.spyOn(console, 'log').mockImplementation(() => null);
     let component: Engine;
 
     beforeEach(() => {
         component = new Engine();
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
-    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+
+    it ('should set engine to "stopped" on creation', () => {
+        expect(component.status).toEqual('stopped')
+    })
+
+    it ('should set engine to "started" on .start()', () => {
+        component.start();
+        expect(component.status).toEqual('started');
+    })
+
+    it ('should set engine to "stopped" after .start() and .stop()', () => {
+        component.start();
+        component.stop();
+        expect(component.status).toEqual('stopped');
+    })
+
+
 });
