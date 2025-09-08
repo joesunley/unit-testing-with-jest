b05aa682da6cca947de5700c3a01d11c71637056
Joe Sunley <jsunley@scottlogic.com>
2025-09-08 10:06:32 +0100

    Complete Task C

--- src/c/component.spec.ts
+++ src/c/component.spec.ts
@@ -10,4 +10,40 @@ describe('SimpleBank', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should throw if deposit is < 0', () => {
+        expect(() => component.deposit(-1)).toThrow();
+    })
+
+    it('should throw if deposit is = 0', () => {
+        expect(() => component.deposit(0)).toThrow();
+    })
+
+    it('should not throw if deposit is > 0', () => {
+        expect(() => component.deposit(1)).not.toThrow();
+    })
+
+    it ('should throw if withdraw is <= 0', () => {
+        expect(() => component.withdraw(-1)).toThrow();
+    })
+
+    it ('should throw if withdraw is = 0', () => {
+        expect(() => component.withdraw(0)).toThrow();
+    })
+
+    it ('should not throw if withdraw is > 0', () => {
+        expect(() => component.withdraw(1)).not.toThrow();
+    })
+
+    it ('should increase balance by 50 to 50', () => {
+        component.deposit(50);
+
+        expect(component.balance).toEqual(50);
+    })
+
+    it ('should decrease balance by 50 to -50', () => {
+        component.withdraw(50);
+
+        expect(component.balance).toEqual(-50);
+    })
 });
