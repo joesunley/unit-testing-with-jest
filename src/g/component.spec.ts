82fad008d332e098462a82dab978960b1c315ea9
Joe Sunley <jsunley@scottlogic.com>
2025-09-08 12:20:06 +0100

    Complete Task G

--- src/g/component.spec.ts
+++ src/g/component.spec.ts
@@ -1,4 +1,4 @@
-import { CustomColumnHeader } from './component';
+import { ColumnHeader, ColumnHeaderParams, CustomColumnHeader } from './component';
 
 describe('CustomColumnHeader', () => {
     let component: CustomColumnHeader;
@@ -7,7 +7,50 @@ describe('CustomColumnHeader', () => {
         component = new CustomColumnHeader();
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
-    });
+    it ('should return headerName', () => {
+        component.init(mockParams());
+
+        expect(component.displayName).toEqual('testHeader');
+    })
+
+    it ('should return undefined for headerName if params is undefined', () => {
+        expect(component.displayName).toBeUndefined();
+    })
+
+    it ('should not be expanded by default if expandable', () => {
+        component.init(mockParams());
+
+        expect(component.isExpanded).toBeFalsy();
+    })
+
+    it ('should not be expanded by default if params is undefined', () => {
+        expect(component.isExpanded).toBeFalsy();
+    })
+
+    it ('should be expanded after toggle', () => {
+        component.init(mockParams());
+        component.toggle();
+
+        expect(component.isExpanded).toBeTruthy();
+    })
+
+    it ('should not be expanded after toggle if params is undefined', () => {
+        component.toggle();
+
+        expect(component.isExpanded).toBeFalsy();
+    })
+
+    function mockParams() {
+        let isExpandable = true;
+        let isExpanded = false;
+
+        return <ColumnHeaderParams>{
+            headerName: 'testHeader',
+            api: {
+                isExpandable: () => isExpandable,
+                isExpanded: () => isExpandable ? isExpanded : false,
+                setExpanded: (expanded: boolean) => isExpanded = expanded,
+            }
+        }
+    }
 });
