c685b25310fb2e4869a47d7c294417a2d7a4b8c3
Joe Sunley <jsunley@scottlogic.com>
2025-09-08 10:58:50 +0100

    Complete Task F

--- src/f/component.spec.ts
+++ src/f/component.spec.ts
@@ -1,4 +1,4 @@
-import { Shop } from './component';
+import { Car, Shop } from './component';
 
 describe('Shop', () => {
     let component: Shop;
@@ -7,7 +7,21 @@ describe('Shop', () => {
         component = new Shop();
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
+    it('should add the expected order', () => {
+        component.newOrder(mockCar(), 6000);
+        
+        const expected = { car: mockCar(), price: 6000 }
+
+        expect(component.orders).toContainEqual(expect.objectContaining(expected));
     });
+
+    function mockCar(): Car {
+        return <Car>{
+            make: 'Ford',
+            model: 'Fiesta',
+            year: 2014,
+            mileage: 60000,
+            color: 'red'
+        }
+    }
 });
